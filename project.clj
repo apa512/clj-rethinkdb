508302d161fd6451062710880c0a8b67492179e4
Erik Strömberg <erik.stromberg@gmail.com>
2014-10-17 21:33:12 +0200

    Add website to project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject rethinkdb "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/apa512/clj-rethinkdb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.json "0.2.5"]
                  [org.flatland/protobuf "0.8.2-SNAPSHOT"]
                  [revise/rethinkdb "1.0.8"]])
