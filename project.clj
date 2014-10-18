a24ac5aeea27875ccd3178e6b10c49389b1b6170
Erik Strömberg <erik.stromberg@gmail.com>
2014-10-18 21:24:35 +0200

    Don't depend on snapshot

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.json "0.2.5"]
-                 [org.flatland/protobuf "0.8.2-SNAPSHOT"]
+                 [org.flatland/protobuf "0.8.1"]
                  [revise/rethinkdb "1.0.8"]])
