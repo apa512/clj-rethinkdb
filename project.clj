47dc829fb602110f83d419d953732b8d0e1a1440
Erik Strömberg <erik.stromberg@gmail.com>
2015-02-14 19:21:41 +0100

    Update docs

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://github.com/apa512/clj-rethinkdb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :codox {:defaults {:doc/format :markdown}}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.json "0.2.5"]
                  [org.flatland/protobuf "0.8.1"]
