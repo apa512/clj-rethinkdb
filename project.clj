cdcd7f7b268af30d8e0f9678a75d3b3dcb47646d
Daniel Compton <desk@danielcompton.net>
2015-11-19 21:09:53 +1300

    Bump version to 0.12.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.apa512/rethinkdb "0.11.0"
+(defproject com.apa512/rethinkdb "0.12.0-SNAPSHOT"
   :description "RethinkDB client"
   :url "http://github.com/apa512/clj-rethinkdb"
   :license {:name "Eclipse Public License"
