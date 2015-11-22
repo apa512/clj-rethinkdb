1cd09c7c8a4ce271a6cd35fdaa023c823b3465e1
Daniel Compton <desk@danielcompton.net>
2015-11-22 22:41:02 +1300

    Use 4 instead of Integer/BYTES in test

--- test/rethinkdb/core_test.clj
+++ test/rethinkdb/core_test.clj
@@ -411,7 +411,7 @@
     (is (contains? server-info :id))
     (is (contains? server-info :name)))
   (is (thrown? ExceptionInfo (r/connect :port 1)))
-  (with-redefs-fn {#'core/send-version (fn [out] (net/send-int out 168696 Integer/BYTES))}
+  (with-redefs-fn {#'core/send-version (fn [out] (net/send-int out 168696 4))}
     #(is (thrown? ExceptionInfo (r/connect)))))
 
 (deftest dont-leak-auth-key
