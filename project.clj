55a90cf40c4f2473b8763dd561eb173ae52ad449
Erik Strömberg <erik.stromberg@gmail.com>
2016-01-20 19:32:45 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.apa512/rethinkdb "0.12.0-SNAPSHOT"
+(defproject com.apa512/rethinkdb "0.12.1-SNAPSHOT"
   :description "RethinkDB client"
   :url "http://github.com/apa512/clj-rethinkdb"
   :license {:name "Eclipse Public License"
