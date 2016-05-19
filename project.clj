c5ad2a0f3690ceac160954becc23275fbd28c6bd
Erik Strömberg <erik.stromberg@gmail.com>
2016-05-19 03:50:42 +0200

    Bad code

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.apa512/rethinkdb "0.15.22"
+(defproject com.apa512/rethinkdb "0.15.23"
   :description "RethinkDB client"
   :url "http://github.com/apa512/clj-rethinkdb"
   :license {:name "Eclipse Public License"
--- src/rethinkdb/net.clj
+++ src/rethinkdb/net.clj
@@ -74,11 +74,10 @@
 
 (defn deliver-result [conn token resp]
   (when-let [{:keys [result cursor]} (get-in @conn [:pending token])]
+    (swap! (:conn conn) update :pending #(dissoc % token))
     (if cursor
-      (do (swap! (:conn conn) update-in [:pending token] #(dissoc % :cursor))
-          (s/put-all! cursor (conj resp ::done)))
-      (do (swap! (:conn conn) update :pending #(dissoc % token))
-          (s/put! result resp)
+      (s/put-all! cursor (conj resp ::done))
+      (do (s/put! result resp)
           (s/close! result)))))
 
 (defn append-result [conn token resp]
