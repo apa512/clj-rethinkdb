e12f74b6d31454922370b208171edcb02c561db7
Adrien Haxaire <adrien@haxaire.org>
2016-08-08 21:16:51 +0200

    Added a test for simple join.
    The creation and deletion of the two tables for the joins is not clean,
    should improve it later on.
    
    Added eq-join test with zip.
    
    And removed zip from the previous test, it's weird it passed in the first place.
    
    Fixed simple eq-join tests.
    
    Decided to move the tests into a separate file because the setup-once
    and setup-each fixtures are different, they need two tables.
    
    Not using optargs.
    
    Fixture refactoring based on code review.
    
    - setup-each creates and passes a connection to reset: https://github.com/apa512/clj-rethinkdb/pull/175#discussion_r73980932
    - delete the tables instead of dropping them: https://github.com/apa512/clj-rethinkdb/pull/175#discussion_r73980593
    
    Use only one connection per test.
    
    Based on code review https://github.com/apa512/clj-rethinkdb/pull/175#discussion_r73980818
    
    Clarify test results.
    
    Comment on why Magnemite is not picked up, answering
    https://github.com/apa512/clj-rethinkdb/pull/175#discussion_r73980770.
    
    TODO: write a test where its types are properly joined.
    
    Reset was still opening a connection.

---
+++ test/rethinkdb/joins_test.clj
@@ -0,0 +1,52 @@
+(ns rethinkdb.joins-test
+  (:require [clojure.test :refer :all]
+            [rethinkdb.query :as r]
+            [rethinkdb.test-utils :as u]))
+
+(def names [{:id 1 :name "Pikachu", :type 2},
+            {:id 2 :name "Magnemite", :type [1 2]}])
+(def types [{:id 1 :type "Steel"},
+            {:id 2 :type "Electric"}])
+
+(defn reset [table data conn]
+  (-> (r/table table)
+      (r/delete {:durability :soft :return-changes false})
+      (r/run conn))
+  (-> (r/table table)
+      (r/insert data)
+      (r/run conn)))
+
+(defn setup-each [test-fn]
+  (with-open [conn (r/connect :db u/test-db)]
+    (reset :names names conn)
+    (reset :types types conn)
+    (test-fn)))
+
+(defn setup-once [test-fn]
+  (with-open [conn (r/connect :db u/test-db)]
+    (u/ensure-db u/test-db conn)
+    (u/ensure-table :names {} conn)
+    (u/ensure-table :types {} conn))
+  (test-fn)
+  (r/run (r/db-drop u/test-db) (r/connect :db u/test-db)))
+
+(deftest eq-join
+  "Magnemite is not picked up because its type is an array of ints instead of an int."
+  (with-open [conn (r/connect :db u/test-db)]
+    (testing
+        "Simple eq-join."
+      (is (=
+           (-> (r/table :names)
+               (r/eq-join :type (r/table :types))
+               (r/run conn)))
+          [{:left {:id 1, :name "Pikachu", :type 2}, :right {:id 2, :type "Electric"}}]))
+
+    (testing "Simple eq-join with zip"
+      (is (-> (r/table :names)
+              (r/eq-join :type (r/table :types))
+              (r/zip)
+              (r/run conn))
+          (= [{:id 1, :name "Pikachu", :type "Electric"}])))))
+
+(use-fixtures :once setup-once)
+(use-fixtures :each setup-each)
